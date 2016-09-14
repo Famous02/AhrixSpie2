4b365e21b79c924606cae21d8bdbfcde35c1120c
Famous02 <casulg3@gmail.com>
2016-09-14 10:34:11 +0800

    Create stdafx.cpp

---
+++ stdafx.cpp
@@ -0,0 +1,7 @@
+//============================================================\\
+// Soul`s Menu Base                                            \\
+// Free To use and mainly for learning                          \\
+// This is not for Just CA will a menu for any game             //
+// This is ONLY the d3d menu no hacks add them yourself ^^     //
+//============================================================//
+#include "stdafx.h"
