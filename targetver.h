81630d5d3f384f1e0d9b3883b807cca2a4560504
Famous02 <casulg3@gmail.com>
2016-09-14 10:35:08 +0800

    Create targetver.h

---
+++ targetver.h
@@ -0,0 +1,22 @@
+//============================================================\\
+// Soul`s Menu Base                                            \\
+// Free To use and mainly for learning                          \\
+// This is not for Just CA will a menu for any game             //
+// This is ONLY the d3d menu no hacks add them yourself ^^     //
+//============================================================//
+#pragma once
+#ifndef WINVER
+#define WINVER 0x0600
+#endif
+
+#ifndef _WIN32_WINNT
+#define _WIN32_WINNT 0x0600
+#endif
+
+#ifndef _WIN32_WINDOWS
+#define _WIN32_WINDOWS 0x0410
+#endif
+
+#ifndef _WIN32_IE
+#define _WIN32_IE 0x0700
+#endif
