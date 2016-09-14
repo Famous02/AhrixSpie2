3e8e3d16f082d2be991f2d5c84f6d58a289117c5
Famous02 <casulg3@gmail.com>
2016-09-14 10:34:34 +0800

    Create stdafx.h

---
+++ stdafx.h
@@ -0,0 +1,18 @@
+//============================================================\\
+// Soul`s Menu Base                                            \\
+// Free To use and mainly for learning                          \\
+// This is not for Just CA will a menu for any game             //
+// This is ONLY the d3d menu no hacks add them yourself ^^     //
+//============================================================//
+#pragma once
+
+#include "targetver.h"
+
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#include <stdio.h>
+#include <d3d9.h>
+#include <d3dx9.h>
+
+#pragma comment(lib, "d3d9.lib")
+#pragma comment(lib, "d3dx9.lib")
